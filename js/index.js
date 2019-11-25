7f2a383fe0562d4bd72777f7abe142eaedaf82f9
Héctor Félix <healfeco@gmail.com>
2019-11-25 12:26:17 -0700

    Added boostrap package, and line to import libraries in index js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css'
 console.log('Welcome the Landing Page');
